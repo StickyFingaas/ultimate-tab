a95423ee7782aa4622005e100ce9037336374200
Aleksa Abramović <60898681+StickyFingaas@users.noreply.github.com>
2021-08-19 15:21:39 +0200

    Express.js server setup.

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,24 @@
-console.log('hello');
+import  express from 'express'
+
+//easily logs http requests
+import morgan from 'morgan'
+//essentially enables resource sharing between backend and frontend
+import cors from 'cors'
+
+//basic express server
+const app = express()
+
+//required string format from morgan API
+app.use(morgan('combined'))
+//easy parsing of json data
+app.use(express.json())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world'
+    })
+})
+const port = process.env.PORT || 8081
+
+app.listen(port)
+
